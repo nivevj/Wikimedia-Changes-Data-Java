8edb4ed2ba3b6e709b8bf9059db6f97980503196
Nivedha <nivedha.vijayakumar@multicorewareinc.com>
2024-10-08 11:41:08 +0530

    Enabled high throughput producer config

--- kafka-producer-wikimedia/src/main/java/io/project/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/project/kafka/wikimedia/WikimediaChangesProducer.java
@@ -27,6 +27,12 @@ public class WikimediaChangesProducer {
         //properties.setProperty(ProducerConfig.ACKS_CONFIG,"all");
         //properties.setProperty(ProducerConfig.RETRIES_CONFIG,Integer.toString(Integer.MAX_VALUE));
 
+        //set high throughput producer configs
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG,"20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG,Integer.toString(32*1024));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG,"snappy");
+
+        //create producer
         KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
 
         String topic = "wikimedia_recentchange";
